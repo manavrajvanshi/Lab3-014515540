ee14d923d202b9d007e9bf96bbe518b41ba19060
Manav Rajvanshi <manavraj96@gmail.com>
2019-11-27 20:54:56 -0800

    Removed the calculator project from lab 3

--- Calculator/frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {Calc} from './calc.js';
-
-ReactDOM.render(<Calc />, document.getElementById('root'));
-
